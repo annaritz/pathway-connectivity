534ce148d7c25114e8e9b8a175fd5ccadc2f7f48
Anna Ritz <aritz@reed.edu>
2019-05-25 15:47:57 -0700

    visualized bipartite graph influence too

--- src/STRING_channels/viz_channels.py
+++ src/STRING_channels/viz_channels.py
@@ -3,6 +3,8 @@ from matplotlib_venn import venn2,venn3
 import glob
 import sys
 import operator 
+import numpy as np
+from scipy.stats import kruskal
 
 ## to draw rectangles
 # https://matplotlib.org/examples/shapes_and_collections/artist_reference.html
@@ -52,6 +54,7 @@ def main(infix):
 		print('DONE WITH %s\n' % (name))
 		#sys.exit()
 	return
+
 
 def viz(interactions,pos_sets,short_names,pos_names,prefix,name,brelax=None):
 	plt.figure(figsize=(10,10))
@@ -255,16 +258,169 @@ def viz(interactions,pos_sets,short_names,pos_names,prefix,name,brelax=None):
 	filename = filename.replace('png','pdf')
 	plt.savefig(filename)
 	print('wrote to '+filename)
+	return
 
-# def get_min_dist(n1_nodes,dist_dict_n1,n2_nodes,dist_dict_n2):
-# 	val = LARGE_VAL
-# 	for n in n2_nodes:
-# 		if dist_dict_n1[n] != None:
-# 			val = min(dist_dict_n1[n],val)
-# 	for n in n1_nodes:
-# 		if dist_dict_n2[n] != None:
-# 			val = min(dist_dict_n2[n],val)
-# 	return val
+def viz_box_plot(interactions,pos_sets,short_names,pos_names,prefix,name,brelax=None):
+	
+	if brelax:
+
+		print('brelax...')
+		### NEW: Make the "Pathway Connected" the universe
+		print('NEW: Make the "Pathway Connected" the universe for plotting.')
+		interactions = {e:interactions[e] for e in pos_sets[2]}
+		pos_sets = pos_sets[2:]
+		pos_names = pos_names[2:]
+		brelax_thresholds = sorted(list(set(list(brelax.values()))))
+		## drop first (bconn) and last (connected) values - these are already considered pos sets.
+		#brelax_thresholds = brelax_thresholds[1:len(brelax_thresholds)-1]
+		print('BRLEAX_thresholds:',brelax_thresholds)
+		brelax_counts = []
+		for b in brelax_thresholds:
+			brelax_counts.append([])
+		for node,val in interactions.items():
+			for i in range(len(brelax_thresholds)):
+				b = brelax_thresholds[i]
+				if node in brelax and brelax[node] <= b:
+					brelax_counts[i].append(node)
+
+		pos_sets = brelax_counts
+		pos_names = ['Pair B-Connected'] + ['B-Dist=%d' % (d) for d in brelax_thresholds[1:-1]] + ['Pair Connected']
+
+		## drop every other if it's too many
+		while len(pos_sets) > 15:
+			if len(pos_sets) > 30:
+				jump = 2
+			else:
+				jump = 3
+
+			pos_sets = [pos_sets[0]] + [pos_sets[i] for i in range(1,len(pos_sets)-1,jump)] + [pos_sets[-1]]
+			pos_names = [pos_names[0]] + [pos_names[i] for i in range(1,len(pos_names)-1,jump)] + [pos_names[-1]]
+	else:
+		print('NEW: Make the "In Anyt Pathway" the universe for plotting.')
+		interactions = {e:interactions[e] for e in pos_sets[0]}
+		pos_sets = pos_sets[1:]
+		pos_names = pos_names[1:]
+	print('PLOTTING %d TOTAL COLS' % (len(pos_sets)))
+	items = sorted(interactions.items(), key=operator.itemgetter(1), reverse=True)
+	vals = []
+	for i in range(len(pos_sets)):
+		vals.append([])
+	print('assigning to positive sets')
+	for i in range(len(pos_sets)):
+		this_set = set(pos_sets[i])
+		vals[i] = [val for edge,val in items if edge in this_set]
+
+	if not brelax:
+		vals = [[i[1] for i in items]] + vals
+		pos_names = ['Pair in Any Pathway'] + pos_names
+	print('done gathering data.')
+
+	if brelax:
+		plt.figure(figsize=(len(pos_sets)*1.2,4))
+	else:
+		plt.figure(figsize=(8,4))
+	#ax1 = plt.subplot(1,2,2)
+	ax1 = plt.subplot(1,1,1)
+	parts = ax1.violinplot(vals,points=1000,showmeans=False, showmedians=False,showextrema=False)
+	labels = ['%s\n($n=%d$)' % (pos_names[i],len(vals[i])) for i in range(len(vals))]
+	if brelax:
+		colors = ['#541684'] + ['#168426']*(len(pos_names)-2) + ['#541684']
+	else:
+		colors = ['#494949','#541684','#541684','#541684']
+	if len(vals) != len(colors):
+		sys.exit('ERROR: len vals is %d and len colors is %d' % (len(vals),len(colors)))
+	format_violin(ax1,parts,vals,labels,colors=colors)
+
+	if not brelax:
+		print('computing KW test')
+		## compute kruskal-wallis test
+		# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kruskal.html
+		pairs = [[1,2],[1,3],[2,3]]
+		shift = max([max(v) for v in vals])/15
+		start_point = max([max(v) for v in vals])+shift
+		for p1,p2 in pairs:
+			statistic,pval = kruskal(vals[p1],vals[p2])
+			#print(p1,p2,statistic,pval)
+			line_x = [p1+1,p1+1,p2+1,p2+1]
+			line_y = [start_point,start_point+shift,start_point+shift,start_point]
+			start_point += shift*2
+			if pval < 0.01:
+				ax1.plot(line_x,line_y,'-k')
+			else:
+				ax1.plot(line_x,line_y,':k')
+			if pval != 0:
+				ax1.text((p1+p2+2)/2,start_point-shift*2,'$p=$%.1e' % (pval),horizontalalignment='center')
+			else:
+				ax1.text((p1+p2+2)/2,start_point-shift*2,'$p<$%.1e' % (sys.float_info.min),horizontalalignment='center')
+		
+	ax1.set_ylabel('Scores')
+	#ax1.set_title('STRING\'s "%s" channel\n(%d interactions map to Reactome)\n' % (name,len(interactions)))
+	ax1.set_title('STRING\'s "%s" channel' % (name),fontsize=14)
+	print('done with the other subplot')
+
+	plt.tight_layout()
+	filename = '%s.png' % (prefix)
+	plt.savefig(filename)
+	print('wrote to '+filename)
+	filename = filename.replace('png','pdf')
+	plt.savefig(filename)
+	print('wrote to '+filename)
+	plt.close()
+	
+	return
+
+def format_violin(ax,parts,vals,labels,colors=None,alpha=.2):
+	for i in range(len(vals)):
+		vals[i] = sorted(vals[i])
+	i=0
+	for pc in parts['bodies']:
+		if not colors:
+			pc.set_facecolor('#541684')
+		else:
+			pc.set_facecolor(colors[i])
+		i+=1
+		pc.set_edgecolor('black')
+		pc.set_alpha(alpha)
+
+	quartile1 = [0]*len(vals)
+	medians = [0]*len(vals)
+	quartile3 = [0]*len(vals)
+	for i in range(len(vals)):
+		quartile1[i], medians[i], quartile3[i] = np.percentile(vals[i], [25, 50, 75])	
+	#quartile1, medians, quartile3 = np.percentile(vals, [25, 50, 75],axis=1)
+	whiskers = np.array([
+		adjacent_values(sorted_array, q1, q3)
+		for sorted_array, q1, q3 in zip(vals, quartile1, quartile3)])
+	whiskersMin, whiskersMax = whiskers[:, 0], whiskers[:, 1]
+
+	
+	for i in range(len(medians)):
+		inds = [(i+1)-.1,(i+1)+.1]
+		meds = [medians[i],medians[i]]
+		ax.plot(inds, meds, color='#ba0000', linestyle='-', lw=5,zorder=3)
+		print('  median:',labels[i],medians[i])
+	inds = np.arange(1, len(medians) + 1)
+	ax.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
+	ax.vlines(inds, whiskersMin, whiskersMax, color='k', linestyle='-', lw=1)
+	
+	set_axis_style(ax, labels)
+	return
+
+# https://matplotlib.org/gallery/statistics/customized_violin.html
+def set_axis_style(ax, labels):
+	ax.get_xaxis().set_tick_params(direction='out')
+	ax.xaxis.set_ticks_position('bottom')
+	ax.set_xticks(np.arange(1, len(labels) + 1))
+	ax.set_xticklabels(labels)
+	ax.set_xlim(0.25, len(labels) + 0.75)
+
+def adjacent_values(vals, q1, q3):
+	upper_adjacent_value = q3 + (q3 - q1) * 1.5
+	upper_adjacent_value = np.clip(upper_adjacent_value, q3, vals[-1])
+
+	lower_adjacent_value = q1 - (q3 - q1) * 1.5
+	lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
+	return lower_adjacent_value, upper_adjacent_value
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
